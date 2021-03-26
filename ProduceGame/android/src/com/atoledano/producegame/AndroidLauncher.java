7fb89f286508ec04c746950906499cc0e3840018
Alejandro Toledano <a.toledanoh@gmail.com>
2021-03-26 16:09:24 -0300

    libgdx setup

---
+++ ProduceGame/android/src/com/atoledano/producegame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.atoledano.producegame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.atoledano.producegame.ProduceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProduceGame(), config);
+	}
+}
---
+++ ProduceGame/desktop/src/com/atoledano/producegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.atoledano.producegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.atoledano.producegame.ProduceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProduceGame(), config);
+	}
+}
